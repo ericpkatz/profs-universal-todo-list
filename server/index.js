20d6421b0f09823cc063a88147b02209c872200a
ericpkatz <ericpkatz@gmail.com>
2022-05-14 06:53:18 -0400

    commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 8080;
+const app = require('./app');
+const db = require('./db');
+const io = require('socket.io');
+
+const init = async () => {
+  await db.syncAndSeed();
+  const server = app.listen(PORT, () =>
+    console.log(`Listening at http://localhost:${PORT}`)
+  );
+  const socketServer = new io.Server(server);
+  socketServer.on('connection', (socket)=> {
+    socket.on('action', (action)=> {
+      socket.broadcast.emit('action', action);
+    });
+  });
+};
+
+init();
